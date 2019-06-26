dde14f3bef4bd72d696af9d0c8096b61cca52546
Nuno Anjos <nmanjos@gmail.com>
2019-06-26 15:05:38 +0100

    added comments

--- ProperWebAPI/Services/IIdentityService.cs
+++ ProperWebAPI/Services/IIdentityService.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace ProperWebAPI.Services
 {
+    //Interface for the Identity Service
     public interface IIdentityService
     {
+        //Registration Method
         Task<AuthenticationResult> RegisterAsync(string email, string password);
 
         Task<AuthenticationResult> LoginAsync(string email, string password);
